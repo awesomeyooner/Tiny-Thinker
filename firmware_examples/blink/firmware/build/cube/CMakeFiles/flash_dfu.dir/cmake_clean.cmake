2f9c2a80c2bd037549466490dd36e9dca93d4d72
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-13 19:46:43 -0700

    removed build folder

--- firmware_examples/blink/firmware/build/cube/CMakeFiles/flash_dfu.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flash_dfu"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flash_dfu.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
